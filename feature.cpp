99511e25ab3f0ab069e57590082f485fee7fc49a
Prasang Agarwal <prasang.agarwal@qbitlabs.co.in>
2023-02-22 14:42:07 +0530

    Added addition of 2 nums

--- feature.cpp
+++ feature.cpp
@@ -10,11 +10,16 @@ using namespace std;
 float cosx(int x){
     return cos(x);
 }
+
+int add2(int x,int y){
+    return x+y;
+}
 int main()
 {
     int a,b;
 
     cout<<cosx(4);
+    cout<<add2(3,5);
     return 0;
 
 
