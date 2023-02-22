dd885d2268c102891c915532f5a2bddae56ee51c
debyanshkumar <debyanshkumar645@gmail.com>
2023-02-22 14:43:02 +0530

    Added multiply2 function

--- feature.cpp
+++ feature.cpp
@@ -10,9 +10,14 @@ int sinX(int x){
     return sin(x);
 }
 
+int multiply2(int a,int b){
+    return a*b;
+}
+
 int main()
 {
     int a,b;
     cout<<sinX(4);
+    cout<<multiply2(4,5);
     return 0;
 }
