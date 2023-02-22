27c2f835404e159b01262331006f281b0c6e0d99
Prasang Agarwal <prasang.agarwal@qbitlabs.co.in>
2023-02-22 14:40:26 +0530

    Added Cosx

--- feature.cpp
+++ feature.cpp
@@ -2,11 +2,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<algorithm>
+#include<math.h>
 
 //Starting new Project
+using namespace std;
 
+float cosx(int x){
+    return cos(x);
+}
 int main()
 {
     int a,b;
+
+    cout<<cosx(4);
     return 0;
+
+
 }
