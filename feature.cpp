9c3761650c8d05977f0b29c4f8896572ec7a3740
debyanshkumar <debyanshkumar645@gmail.com>
2023-02-22 14:41:13 +0530

    Added SinX functon

--- feature.cpp
+++ feature.cpp
@@ -2,11 +2,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<algorithm>
-
+#include<math.h>
+using namespace std;
 //Starting new Project
+
+int sinX(int x){
+    return sin(x);
+}
 
 int main()
 {
     int a,b;
+    cout<<sinX(4);
     return 0;
 }
